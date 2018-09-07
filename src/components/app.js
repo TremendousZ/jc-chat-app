b23322f67643db52109842d124c83763a5f325e0
John Carlisle <jmcarlisle@live.com>
2018-09-07 12:55:38 -0700

    chat routing setup

--- src/components/app.js
+++ src/components/app.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import { Route } from 'react-router-dom';
 import Home from './home';
+import Chat from './chat';
 
 
 
 const App = () => (
     <div className = 'container'>
         <Route path ='/' exact component = {Home} />
+        <Route path ='/chat' component = {Chat} />
     </div>
 );
 
