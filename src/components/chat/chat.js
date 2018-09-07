c63be242458eabae9438a6cb75d85b5391d15b88
John Carlisle <jmcarlisle@live.com>
2018-09-07 16:15:46 -0700

    fetched data

--- src/components/chat/chat.js
+++ src/components/chat/chat.js
@@ -1,20 +1,53 @@
 import React , {Component} from 'react';
 import './chat.css';
 import { Link } from 'react-router-dom';
+import {db} from '../../firebase';
+import { connect } from 'react-redux';
+import { updateChat } from '../../actions';
+import './chat.css';
 
 class Chat extends Component {
+    dbRef = db.ref('/');
+
+    componentDidMount(){
+        this.dbRef.on('value', this.props.updateChat);
+    }
+    componentWillUnmount(){
+        this.dbRef.off();
+    }
+
     render(){
+        const { log } = this.props;
+
+        const chatElement = Object.keys(log).map(k => {
+            const { name, message } = log[k];
+            return (
+                <li className = 'collection-item' key = {k} >
+                    <b> {name}:</b> { message }
+                </li>
+            )
+        });
+
         return ( 
             <div>
                 <h1 className = 'center'> Chat Room </h1>
                 <div className="row right-align">
                     <Link to = '/' className='btn teal accent-3'>Home</Link>
                 </div> 
+                <ul className = 'collection'>
+                    {chatElement}
+                </ul>
             </div>
         )
 
     }
 }
 
-export default Chat;
+function mapStateToProps(state){
+    return {
+         log:state.chat.log
+    }    
+}
+
+export default connect(mapStateToProps, {updateChat})(Chat);
 
